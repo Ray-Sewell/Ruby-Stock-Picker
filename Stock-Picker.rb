15c8edc18a5a442800d17931161ed9637de85612
Ray <sewellray11+git@googlemail.com>
2020-12-29 14:35:16 +0000

    Removed useless Broker class

--- Stock-Picker.rb
+++ Stock-Picker.rb
@@ -1,23 +1,12 @@
-class Broker
-    attr_reader :stockStats, :stockList
-    def initialize(stockList)
-        @stockList = stockList
-        @stockStats = {
-            lowStockIndex: nil,
-            highStockIndex: nil
-        }
-    end
-
-    def analyze
-        lowDay = nil, highDay = nil, value = 0
-        stockList.each_with_index{|stock, i| stockList[i..].each_with_index{|comparison, c| 
-        if (comparison - stock) > value
-            lowDay = i
-            highDay = c + i
-            value = (comparison - stock)
-        end}}
-        puts "Buy on day #{lowDay} and sell on day #{highDay} for £#{value}!"
-    end
+def analyze(stockList)
+    lowDay = nil, highDay = nil, value = 0
+    stockList.each_with_index{|stock, i| stockList[i..].each_with_index{|comparison, c| 
+    if (comparison - stock) > value
+        lowDay = i
+        highDay = c + i
+        value = (comparison - stock)
+    end}}
+    puts "Buy on day #{lowDay} and sell on day #{highDay} for £#{value}!"
 end
 
-Broker.new([17,3,6,9,15,8,6,1,10]).analyze
+analyze([17,3,6,9,15,8,6,1,10])
